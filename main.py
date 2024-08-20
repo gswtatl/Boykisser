50ebe74887aa65a94425a09367fadbd328b1c82a
gswtatl <gswtatl@gmail.com>
2024-08-20 19:46:49 +0300

    plan & assets

--- main.py
+++ main.py
@@ -4,21 +4,42 @@ pygame.init()
 
 window = pygame.display.set_mode((700, 500))
 pygame.display.set_caption('Boykisser')
-clock = pygame.time.Clock()
+fps = pygame.time.Clock()
+
+'''
+бэк можно написать в пару строк в осн цикле
+
+сцена 1:
+    1. старт
+    2. настройки (апцианально!)
+    -> выбор песни
+    -> громкость
+сцена 2:
+    если (нюша смотрит -> через рандом ай гесс) и (нажмали пробел или лкм):
+        сцена 3:
+            проиграли -> заново?
+                -> да: сцена 2
+                -> нет: сцена 1
+'''
 
 
-def draw(window):
-    # Background
+def start_scene():
     pass
 
 
+def game_scene():
+    pass
+
+
+def defeat_scene():
+    pass
+
+    
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             __import__('sys').exit()
 
-    draw(window)
-
     pygame.display.update()
-    clock.tick(60)
+    fps.tick(60)
