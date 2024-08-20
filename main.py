9942337c8a471e3c0cb2a8ebb639badadfc34892
gswtatl <gswtatl@gmail.com>
2024-08-21 00:41:22 +0300

    smth like a start menu

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-'''
-бэк можно написать в пару строк в осн цикле
-
-сцена 1:
-    старт
-сцена 2:
-    если (нюша смотрит -> через рандом?) и (нажмали пробел или лкм):
-        сцена 3:
-            проиграли -> заново?
-                -> да: сцена 2
-                -> нет: сцена 1
-'''
-
 import pygame
 
 pygame.init()
@@ -20,25 +7,31 @@ pygame.display.set_caption('Boykisser')
 fps = pygame.time.Clock()
 
 
-def start_scene():
-    pass
-
+def start_menu():
+    window.fill('#f5f5dc')
+    font = pygame.font.Font(r'python\kissing_game\assets\font.TTF', 80)
+    game_name = font.render('Boykisser', True, '#002137')
+    window.blit(game_name, game_name.get_rect(center=(350, 150)))
+    button_text = font.render('пробел пж', True, 'white', '#1fcecb')
+    window.blit(button_text, button_text.get_rect(center=(350, 350)))
 
 def game_scene():
-    pass
+    bg = pygame.image.load(r'python\kissing_game\assets\bg.png')
+    window.blit(bg, (0, 0))
 
 
-def defeat_scene():
-    pass
-
-    
+run = False
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             __import__('sys').exit()
-
-
+        if not run:
+            start_menu()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                run = True
+        else:
+            game_scene()
 
     pygame.display.update()
     fps.tick(60)
