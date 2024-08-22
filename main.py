441c4e0133d26c18d87a23ababb0c653713d2b79
gswtatl <gswtatl@gmail.com>
2024-08-23 01:15:28 +0300

    new assets + classes

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 import pygame
 
-pygame.init()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.player_afk = pygame.image.load(r'python\\kissing_game\\assets\\player_afk.png').convert_alpha()
+        self.player_caught = pygame.image.load(r'python\\kissing_game\\assets\\player_caught.png').convert_alpha()
+        self.player_kiss = pygame.image.load(r'python\\kissing_game\\assets\\player_kiss.png').convert_alpha()
+        self.image = self.player_afk
+        self.rect = self.image.get_rect(midbottom=(400, 400))
+
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        while keys[pygame.K_SPACE] == True:
+            self.image = self.player_kiss
+    
+    def update(self):
+        self.player_input()
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+
+pygame.init()
 window = pygame.display.set_mode((700, 500))
 pygame.display.set_caption('Boykisser')
-fps = pygame.time.Clock()
+icon = pygame.image.load(r'python\\kissing_game\\icon.png')
+pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
 
+# Groups
+player = pygame.sprite.GroupSingle()
+player.add(Player())
 
-def start_menu():
-    window.fill('#f5f5dc')
-    font = pygame.font.Font(r'python\kissing_game\assets\font.TTF', 80)
-    game_name = font.render('Boykisser', True, '#002137')
-    window.blit(game_name, game_name.get_rect(center=(350, 150)))
-    button_text = font.render('пробел пж', True, 'white', '#1fcecb')
-    window.blit(button_text, button_text.get_rect(center=(350, 350)))
+enemy = pygame.sprite.GroupSingle()
+enemy.add(Enemy())
 
-def game_scene():
-    bg = pygame.image.load(r'python\kissing_game\assets\bg.png')
-    window.blit(bg, (0, 0))
-
-
-run = False
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             __import__('sys').exit()
-        if not run:
-            start_menu()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                run = True
-        else:
-            game_scene()
+    
+    bg = pygame.image.load(r'python\\kissing_game\\assets\\bg.png')
+    window.blit(bg, (0, 0))
+    
+    player.draw(window)
+    player.update()
 
     pygame.display.update()
-    fps.tick(60)
+    clock.tick(60)
